f50bba3725550e3c25f24384b86be1f776a5b032
Matin <matin.parsapour.iam@gmail.com>
2023-03-08 12:06:38 +0330

    Update module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntrancementComponent } from './components/entrancement/entrancement.component';
+import { PhoneNumberComponent } from './components/phone-number/phone-number.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrancementComponent,
+    PhoneNumberComponent
   ],
   imports: [
     BrowserModule,
