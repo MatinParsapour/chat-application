2447d57a3b915efe6c3f35931b034927411df549
Matin <matin.parsapour.iam@gmail.com>
2023-03-08 14:21:11 +0330

    Add routing for user entrance

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrancementComponent } from './components/entrancement/entrancement.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: EntrancementComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
