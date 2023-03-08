93de4ae5d549f641a3aaf64e140ced6742f8af3c
Matin <matin.parsapour.iam@gmail.com>
2023-03-08 14:22:35 +0330

    Update phone number component

--- src/app/components/phone-number/phone-number.component.ts
+++ src/app/components/phone-number/phone-number.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, FormGroupDirective } from '@angular/forms';
+import { ISO_3166_1_CODES } from 'src/app/model/country-code/country-codes';
 
 @Component({
   selector: 'app-phone-number',
   templateUrl: './phone-number.component.html',
-  styleUrls: ['./phone-number.component.css']
+  styleUrls: ['./phone-number.component.css'],
 })
 export class PhoneNumberComponent implements OnInit {
+  @ViewChild('phone') phone!: ElementRef;
+  @Output() updateUserPhone: EventEmitter<string> = new EventEmitter<string>();
+  countyCodes = ISO_3166_1_CODES;
+  example: string = 'e.g. --- --- ---';
+  phoneInput!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private rootFormGroup: FormGroupDirective
+  ) {}
 
   ngOnInit(): void {
+    this.phoneInput = this.rootFormGroup.control;
+    this.phoneInput.get('country')?.valueChanges.subscribe((data) => {
+      this.phone.nativeElement.style.width = String(data).length + 'em';
+    });
   }
 
+  changeInput() {
+    this.updateUserPhone.emit(
+      this.phoneInput.get('phone.country')?.value +
+        this.phoneInput.get('phone.number')?.value
+    );
+  }
 }
