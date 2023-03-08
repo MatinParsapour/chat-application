96109eadda054cc43cb0b5788e8cb163a4b70b90
Matin <matin.parsapour.iam@gmail.com>
2023-03-08 14:22:12 +0330

    Update entrancement component

--- src/app/components/entrancement/entrancement.component.ts
+++ src/app/components/entrancement/entrancement.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl, FormBuilder } from '@angular/forms';
+import { User } from 'src/app/model/user/user.model';
 
 @Component({
   selector: 'app-entrancement',
   templateUrl: './entrancement.component.html',
-  styleUrls: ['./entrancement.component.css']
+  styleUrls: ['./entrancement.component.css'],
 })
 export class EntrancementComponent implements OnInit {
+  user: User = new User();
+  userPhone = this.formBuilder.group({
+    phone: this.formBuilder.group({
+      country: ['+98', Validators.required],
+      number: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5),
+      ]),
+    }),
+  });
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  updateUserPhoneNumber(phone: string) {
+    this.user.phone = phone;
   }
 
+  submit() {
+    console.log(this.user.phone);
+  }
 }
